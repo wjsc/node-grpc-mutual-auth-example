bf122193e1fdf72f46379cabedcfc9102551d9c0
Walter Scarano <walter.scarano@naranjax.com>
2020-10-07 20:16:54 -0300

    Working version

---
+++ client.js
@@ -0,0 +1,49 @@
+const fs = require("fs");
+const path = require("path");
+const protoLoader = require("@grpc/proto-loader");
+const grpc = require("grpc");
+
+const packageDefinition = protoLoader.loadSync("helloworld.proto");
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
+
+const options = {
+  'grpc.ssl_target_name_override' : 'login.services.widgets.inc',
+  'grpc.default_authority': 'login.services.widgets.inc'
+};
+
+function main() {
+  let client;
+
+  if (process.env.NODE_ENV === "production") {
+    const certsDir = path.join(process.cwd(), "client-certs");
+
+    client = new hello_proto.Greeter(
+      "localhost:50051",
+      grpc.credentials.createSsl(
+        fs.readFileSync(path.join(certsDir, "Snazzy_Microservices.crt")),
+        fs.readFileSync(path.join(certsDir, "client-1010101.key")),
+        fs.readFileSync(path.join(certsDir, "client-1010101.crt"))
+      ),
+      options
+    );
+  } else {
+    client = new hello_proto.Greeter(
+      "localhost:50051",
+      grpc.credentials.createInsecure()
+    );
+  }
+
+  client.sayHello({ name: "you" }, function(err, response) {
+    err && console.error(err);
+
+    console.log("Greeting:", response.message);
+  });
+
+  client.sayHelloAgain({ name: "you" }, function(err, response) {
+    err && console.error(err);
+
+    console.log("Greeting:", response.message);
+  });
+}
+
+main();
